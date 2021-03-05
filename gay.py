eef66bd7559ff21cedb82f3c0fb2fb3ff6731b19
Matias <none>
2021-03-04 23:43:57 -0600

    fix bug

--- gay.py
+++ gay.py
@@ -17,17 +17,17 @@ class Gay(commands.Cog):
     async def gay(self, ctx: commands.Context):
         try:
             await ctx.message.delete()
-        except commands.MissingPermissions:
+        except (commands.MissingPermissions, discord.errors.NotFound):
             await ctx.channel.send(config.NO_PERMISSIONS)
             return
 
         gay1 = await ctx.send(file=discord.File(config.GAY1))
 
         def check(msg):
-            return msg.channel == ctx.channel
+            return not msg.author.bot and msg.channel == ctx.channel
 
         try:
-            new_message = await self.bot.wait_for('message', check=check, timeout=30)
+            new_message = await self.bot.wait_for('nocommand', check=check, timeout=30)
         except asyncio.TimeoutError:
             await gay1.delete()
         else:
