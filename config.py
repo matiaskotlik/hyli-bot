d65c61e1eba87d13dc77abb335a491fbcbf524d8
Matias <none>
2021-03-11 13:46:30 -0600

    edit yuubee regex

--- config.py
+++ config.py
@@ -43,7 +43,7 @@ REACTS: list[tuple[Optional[list[int]], re.Pattern, list[str]]] = [
     ([315898478712717312], re.compile(r'\blmf?a+o+\b', re.IGNORECASE), ['<:lmao:804387193506103316>']), # abhishek lmao
     ([229713250793553922], re.compile(r'\bcum(ming)?\b', re.IGNORECASE), ['<:cum:819649767666614303>']), # raghav cum
     ([382674822926434335], re.compile(r'\bmm+\s+penis\b', re.IGNORECASE), ['🤤', '🍆']), # violet mmm penis
-    ([382674822926434335], re.compile(r'\byubee\b', re.IGNORECASE), ['😺', '🐝']), # violet yubee
+    ([382674822926434335], re.compile(r'\byu+bee\b', re.IGNORECASE), ['😺', '🐝']), # violet yubee
     ([224292077868023809], re.compile(r'\bju?n?gl?e?\W*(dif|gap)', re.IGNORECASE), ['<:jgdif:818221968297295928>']), # matias jgdif
     ([180439899567030272], re.compile(r'\b(fem)?(boy\s*)?cock\b', re.IGNORECASE), ['🍆']), # zapata eggplant
     (None, re.compile(r'\bsmoger?\b', re.IGNORECASE), [r'<:sadge:753638806460039218>', '🚬']), # smoge
